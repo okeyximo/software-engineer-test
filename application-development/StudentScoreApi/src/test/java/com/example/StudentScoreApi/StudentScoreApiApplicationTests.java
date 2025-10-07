46e4354b25e9bc13208ce9ffa82c631ffa4edf75
Onwuchekwa Okechukwu <okey.onwuchekwa@excelfore.com>
2025-10-07 02:46:52 +0100

    Initialize student score API with Gradle setup, Docker configuration, and application properties

---
+++ application-development/StudentScoreApi/src/test/java/com/example/StudentScoreApi/StudentScoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudentScoreApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentScoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
